d6f3b3e834d87303023e302ff252e4457c5e3075
marialediazr <marialediazr@gmail.com>
2024-12-04 09:08:09 +0000

    Update Uber.py

--- Uber.py
+++ Uber.py
@@ -1,80 +1,130 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[14]:
+# In[3]:
 
 
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import chardet
 
 
-# In[15]:
+# In[2]:
 
 
 st.title("Dashboard")
 
 
-# In[16]:
+# In[3]:
 
 
 DATE_COLUMN="date/time"
 
 
-# In[17]:
+# In[4]:
 
 
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+with open('movies.csv', 'rb') as f:
+    raw_data = f.read()
+    result = chardet.detect(raw_data)
+    print(result) 
 
 
-# In[19]:
+# In[5]:
 
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase=lambda x:str(x).lower()
-    data.rename(lowercase, axis="columns", inplace=True)
-    data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
-    return data
+movies = pd.read_csv('movies.csv', encoding='ISO-8859-1')
+
+movies.to_csv('movies_utf8.csv', index=False, encoding='utf-8')
 
 
-# In[20]:
+# In[6]:
 
 
-data_load_state=st.text("Loading Data...")
-data=load_data(1000)
-data_load_state.text("Loading Data...Done! using cache_data")
+movies.head(15)
 
 
-# In[21]:
+# In[7]:
 
 
-if st.checkbox("Show Raw Data"):
-    st.subheader("Raw Data")
-    st.write(data)
+movies['year'] = movies['title'].str.extract(r'\((\d{4})\)')
+movies.head()
 
 
-# In[24]:
+# In[8]:
 
 
-st.subheader("Number of Pickups per hour")
-hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
-st.bar_chart(hist_values)
+ratings = pd.read_csv("ratings.csv")
 
 
-# In[25]:
+# In[9]:
 
 
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
- 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+with open('tags.csv', 'rb') as f:
+    raw_data = f.read()
+    result = chardet.detect(raw_data)
+    print(result) 
 
 
-# In[ ]:
+# In[10]:
 
 
+tags = pd.read_csv('tags.csv', encoding='Windows-1252')
 
+tags.to_csv('tags_utf8.csv', index=False, encoding='utf-8')
+
+
+# In[11]:
+
+
+average_ratings = ratings.groupby('movieId')['rating'].mean().reset_index()
+average_ratings.columns = ['movieId', 'average_rating']
+average_ratings['average_rating'] = average_ratings['average_rating'].round(1)
+print(average_ratings)
+
+
+# In[12]:
+
+
+movies2 = movies.copy()
+
+
+# In[13]:
+
+
+movies2.drop('title', axis=1, inplace=True)
+
+
+# In[14]:
+
+
+movies2.head()
+
+
+# In[15]:
+
+
+movies2['year'] = movies2['year'].astype(int)
+
+
+# In[16]:
+
+
+movies_per_year = movies2.groupby('year').size()
+
+plt.figure(figsize=(10, 6))
+plt.plot(movies_per_year.index, movies_per_year.values, marker='o')
+plt.title('Number of Movies Released Per Year')
+plt.xlabel('Year')
+plt.ylabel('Number of Movies')
+
+custom_ticks = list(range(movies_per_year.index.min(), movies_per_year.index.max() + 1, 5))
+plt.xticks(custom_ticks)
+plt.xticks(rotation=90, fontsize=8)
+
+plt.grid(True)
+plt.show()
 
